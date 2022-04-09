fc7a1e7cf680bd0b2141b404cd3c03e24aed2be8
Manoj Puri <maxepuri@gmail.com>
2022-04-09 11:56:41 +0530

    After gipper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, RouterModule.forRoot([
+      {path: 'myprofile', component: MyProfileComponent},
+      
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
